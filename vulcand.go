d94bd0b1298dfe68041c59934b48c49dcc6c7cd7
Marvin Winkler (Admin) <m@rvin.at>
2014-12-07 11:29:08 +0100

    fixed vulcand.go to finally get it working

--- vulcand.go
+++ vulcand.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net"
 	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/mailgun/vulcand/api"
@@ -14,28 +15,31 @@ type VulcandRegistry struct {
 }
 
 func NewVulcandRegistry(uri *url.URL) ServiceRegistry {
-	url string
+	var url string
+	var registry *plugin.Registry
+	
 	if uri.Host != "" {
-		url = uri.URL.String()
+		url = uri.String()
 	}
+	
 	return &VulcandRegistry{client: api.NewClient(url, registry)}
 }
 
 func (r *VulcandRegistry) Register(service *Service) error {
-	addr := net.JoinHostPort(service.IP, port)
 	port := strconv.Itoa(service.Port)
+	addr := net.JoinHostPort(service.IP, port)
 
-	upstreamId = strings.Split(service.Name, "-")[0]
-	id = service.ID
-	u = "http://" + addr + ":" + port
+	upstreamId := getUpstreamId(service.Name)
+	id := service.ID
+	u := "http://" + addr + ":" + port
 
 	_, err := r.client.AddEndpoint(upstreamId, id, u)
 	return err
 }
 
 func (r *VulcandRegistry) Deregister(service *Service) error {
-	upstreamId = strings.Split(service.Name, "-")[0]
-	id = service.ID
+	upstreamId := getUpstreamId(service.Name)
+	id := service.ID
 
 	_, err := r.client.DeleteEndpoint(upstreamId, id)
 	return err
@@ -43,4 +47,13 @@ func (r *VulcandRegistry) Deregister(service *Service) error {
 
 func (r *VulcandRegistry) Refresh(service *Service) error {
 	return r.Register(service)
+}
+
+
+func getUpstreamId(name string) string {    
+  upstreamId := name
+  if(strings.ContainsAny(name, "-")){
+    upstreamId = strings.Split(name, "-")[0]
+  }
+  return upstreamId
 }
