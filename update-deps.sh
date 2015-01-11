a6a13aaee2040c4daf63c95ecae885b5757e84aa
Brian Lalor <blalor@bravo5.org>
2015-01-11 08:22:11 -0500

    Improved Makefile
    Does not assume godep is already installed.  Also adds target and
    script for updating godep dependencies.

---
+++ update-deps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## helper script for updating dependencies; too much shell-fu for a Makefile
+## expects GOPATH to be set, requires godep
+
+set -e
+
+export PATH="${GOPATH}/bin:${PATH}"
+
+## update existing deps; exclude built-in packages
+go list -f '{{ range .Imports }}{{.}} {{end}}' \
+    | tr ' ' '\n' \
+    | egrep '\.(com|org|net)/' \
+    | while read x; do
+        echo "==> updating $x"
+        godep update $x
+    done
+
+## save new deps pulled in by updates
+godep save
