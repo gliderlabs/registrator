4246347e74d4da4271fdcde3e073244f329f83f6
mijime <07cc0110@gmail.com>
2015-07-09 23:40:58 +0900

    Fix the namespace of the module in the correct thing

--- modules.go
+++ modules.go
@@ -3,7 +3,7 @@ package main
 import (
 	_ "github.com/gliderlabs/registrator/consul"
 	_ "github.com/gliderlabs/registrator/consulkv"
+	_ "github.com/gliderlabs/registrator/dynamodb"
 	_ "github.com/gliderlabs/registrator/etcd"
 	_ "github.com/gliderlabs/registrator/skydns2"
-	_ "github.com/mijime/registrator/dynamodb"
 )
