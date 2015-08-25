166b6c062d2888f3f5a76a6d76b178e0baddbd32
John Days <johnydays0@gmail.com>
2015-08-25 19:28:35 +0100

    Added a new backend consulmeta:

--- modules.go
+++ modules.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "github.com/gliderlabs/registrator/consul"
 	_ "github.com/gliderlabs/registrator/consulkv"
+	_ "github.com/gliderlabs/registrator/consulmeta"
 	_ "github.com/gliderlabs/registrator/etcd"
 	_ "github.com/gliderlabs/registrator/skydns2"
 )
