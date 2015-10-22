af92d0a84bd0926f9d115ffc273631d4dd1bf189
Jean-François Roche <jfroche@affinitic.be>
2015-10-23 00:56:30 +0200

    drop formatted_ip variable

--- consul/consul.go
+++ consul/consul.go
@@ -67,11 +67,10 @@ func (r *ConsulAdapter) Register(service *bridge.Service) error {
 func (r *ConsulAdapter) buildCheck(service *bridge.Service) *consulapi.AgentServiceCheck {
 	check := new(consulapi.AgentServiceCheck)
 	if path := service.Attrs["check_http"]; path != "" {
-		formatted_ip := service.IP
 		if net.ParseIP(service.IP).To4() == nil { // verify if its an ipv6 address
-			formatted_ip = fmt.Sprintf("[%s]", service.IP)
+			service.IP = fmt.Sprintf("[%s]", service.IP)
 		}
-		check.HTTP = fmt.Sprintf("http://%s:%d%s", formatted_ip, service.Port, path)
+		check.HTTP = fmt.Sprintf("http://%s:%d%s", service.IP, service.Port, path)
 		if timeout := service.Attrs["check_timeout"]; timeout != "" {
 			check.Timeout = timeout
 		}
