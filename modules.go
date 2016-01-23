9e8ea18f72ee44629d2e357c71e34076da966546
Rudolf Randal <rudolf@openfirm.io>
2016-01-23 03:23:28 +0100

    fix changed name

--- modules.go
+++ modules.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	_ "github.com/RudolfRandal/registrator/consul"
-	_ "github.com/RudolfRandal/registrator/consulkv"
-	_ "github.com/RudolfRandal/registrator/etcd"
-	_ "github.com/RudolfRandal/registrator/skydns2"
-	_ "github.com/RudolfRandal/registrator/skydns2s"
-	_ "github.com/RudolfRandal/registrator/zookeeper"
+	_ "github.com/rudolfrandal/registrator/consul"
+	_ "github.com/rudolfrandal/registrator/consulkv"
+	_ "github.com/rudolfrandal/registrator/etcd"
+	_ "github.com/rudolfrandal/registrator/skydns2"
+	_ "github.com/rudolfrandal/registrator/skydns2s"
+	_ "github.com/rudolfrandal/registrator/zookeeper"
 )
