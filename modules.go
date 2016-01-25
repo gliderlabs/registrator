3360c4de9a485ce45155604defccee48b4623c2c
Rudolf Randal <rudolf@openfirm.io>
2016-01-25 07:52:21 +0100

    remove import of skydns2s

--- modules.go
+++ modules.go
@@ -5,6 +5,5 @@ import (
 	_ "github.com/rudolfrandal/registrator/consulkv"
 	_ "github.com/rudolfrandal/registrator/etcd"
 	_ "github.com/rudolfrandal/registrator/skydns2"
-	_ "github.com/rudolfrandal/registrator/skydns2s"
 	_ "github.com/rudolfrandal/registrator/zookeeper"
 )
