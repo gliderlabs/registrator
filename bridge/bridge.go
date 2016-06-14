5d649d037fc0faf2aa40769eabc78c23cbeec0c0
Vadzim Zapolski <vadzim.zapolski@pipedrive.com>
2016-06-14 14:51:52 +0300

    hostname-container tag is provided now

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -263,11 +263,11 @@ func (b *Bridge) newService(port ServicePort, isgroup bool) *Service {
 
 	if port.PortType == "udp" {
 		service.Tags = combineTags(
-			mapDefault(metadata, "tags", ""), b.config.ForceTags, "udp", hostname)
+			mapDefault(metadata, "tags", ""), b.config.ForceTags, "udp", hostname + "-" + container.Config.Hostname)
 		service.ID = service.ID + ":udp"
 	} else {
 		service.Tags = combineTags(
-			mapDefault(metadata, "tags", ""), b.config.ForceTags, hostname)
+			mapDefault(metadata, "tags", ""), b.config.ForceTags, hostname + "-" + container.Config.Hostname)
 	}
 
 	id := mapDefault(metadata, "id", "")
