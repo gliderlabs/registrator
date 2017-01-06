b59e942b7630552c6716806650a8183165115106
Chris Gilbert <chris.gilbert@hudl.com>
2017-01-06 13:59:22 +0000

    Remove unneeded interfaces

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,28 +1,6 @@
 package interfaces
 
-import (
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
-	fargo "github.com/hudl/fargo"
-)
-
-// InstanceData - Type to wrap fargo.Instance for testing
-type InstanceData struct {
-	fargo.Instance
-	// SetMetadataString(string, string)
-	// Id() string
-}
-
-// RegistrationData - Wrap registration instance to facilitate testing
-type RegistrationData struct {
-	Instance InstanceData
-}
-
-// EurekaConnector - Wraps fargo.EurekaConnection to facilitate testing
-type EurekaConnector interface {
-	HeartBeatInstance(*InstanceData)
-	GetApp(string)
-	DeregisterInstance(*InstanceData)
-}
+import "github.com/aws/aws-sdk-go/aws/ec2metadata"
 
 // EC2MetadataGetter Interface to help with test mocking
 type EC2MetadataGetter interface {
