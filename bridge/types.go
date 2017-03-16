3ea50e9b4732eff1031bad783b6b6220b7882810
Alexander Block <ablock84@gmail.com>
2017-03-16 13:57:47 +0100

    Add option to only register services which have SERVICE_NAME
    This adds the option "-explicit" which makes registrator ignore all
    services which don't have a SERVICE_NAME label.

--- bridge/types.go
+++ bridge/types.go
@@ -22,6 +22,7 @@ type RegistryAdapter interface {
 type Config struct {
 	HostIp          string
 	Internal        bool
+	Explicit        bool
 	UseIpFromLabel  string
 	ForceTags       string
 	RefreshTtl      int
