b054ab6cc507b30dcae640008011f518c65105a1
Marcus Linke <linke@newsaktuell.de>
2017-09-29 22:03:41 +0200

    Optional swarm manager service registration

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -15,19 +15,28 @@ import (
 )
 
 // format: node_id:container_name:port(:"upd")
-var containerServiceIDPattern = regexp.MustCompile(`^(.+?):([a-zA-Z0-9][a-zA-Z0-9_.-]+):[0-9]+(?::udp)?$`)
+var containerSvcIDPattern = regexp.MustCompile(`^(.+?):([a-zA-Z0-9][a-zA-Z0-9_.-]+):[0-9]+(?::udp)?$`)
 
-// format: "swarm":swarm_service_id:node_id:port(:"upd")
-var swarmServiceIDPattern = regexp.MustCompile(`^swarm:(.+?):([a-zA-Z0-9][a-zA-Z0-9_.-]+):[0-9]+(?::udp)?$`)
+// format: "swarm-vip-svc":swarm_service_id:node_id:port(:"upd")
+var swarmVipSvcIDPattern = regexp.MustCompile(`^swarm-vip-svc:(.+?):([a-zA-Z0-9][a-zA-Z0-9_.-]+):[0-9]+(?::udp)?$`)
+
+// format: "swarm-mgr-svc":node_id:port
+var swarmMgrSvcIDPattern = regexp.MustCompile(`^swarm-mgr-svc:(.+?):[0-9]+$`)
+
 
 type Bridge struct {
 	sync.Mutex
-	registry           RegistryAdapter
-	docker             *dockerapi.Client
-	containerServices  map[string][]*Service
-	deadContainers     map[string]*DeadContainer
-	swarmServices      map[string][]*Service
-	config             Config
+	registry              RegistryAdapter
+	docker                *dockerapi.Client
+	// containerId -> services
+	containerServices     map[string][]*Service
+	deadContainers        map[string]*DeadContainer
+	// swarmServiceId -> services
+	swarmVipServices      map[string][]*Service
+	// swarmNodeId -> services
+	swarmMgrServices      map[string][]*Service
+	config                Config
+	swarmControlAvailable bool
 }
 
 func New(docker *dockerapi.Client, adapterUri string, config Config) (*Bridge, error) {
@@ -41,13 +50,14 @@ func New(docker *dockerapi.Client, adapterUri string, config Config) (*Bridge, e
 	}
 
 	log.Println("Using", uri.Scheme, "adapter:", uri)
+
 	return &Bridge{
-		docker:            docker,
-		config:            config,
-		registry:          factory.New(uri),
-		containerServices: make(map[string][]*Service),
-		deadContainers:    make(map[string]*DeadContainer),
-		swarmServices:     make(map[string][]*Service),
+		docker:                docker,
+		config:                config,
+		registry:              factory.New(uri),
+		containerServices:     make(map[string][]*Service),
+		deadContainers:        make(map[string]*DeadContainer),
+		swarmVipServices:      make(map[string][]*Service),
 	}, nil
 }
 
@@ -69,6 +79,19 @@ func (b *Bridge) RemoveOnExit(containerId string) {
 	b.remove(containerId, b.shouldRemove(containerId))
 }
 
+func (b *Bridge) refreshServices(servicesMap map[string][]*Service) {
+	for _, services := range servicesMap {
+		for _, service := range services {
+			err := b.registry.Refresh(service)
+			if err != nil {
+				log.Printf("service %s refresh failed: %v", service.ID, err)
+				continue
+			}
+			log.Printf("refreshed: service %s", service.ID)
+		}
+	}
+}
+
 func (b *Bridge) Refresh() {
 	b.Lock()
 	defer b.Unlock()
@@ -80,27 +103,9 @@ func (b *Bridge) Refresh() {
 		}
 	}
 
-	for containerId, services := range b.containerServices {
-		for _, service := range services {
-			err := b.registry.Refresh(service)
-			if err != nil {
-				log.Println("refresh container service failed:", containerId[:12], service.ID, err)
-				continue
-			}
-			log.Println("refreshed: container service ", containerId[:12], service.ID)
-		}
-	}
-
-	for swarmServiceId, services := range b.swarmServices {
-		for _, service := range services {
-			err := b.registry.Refresh(service)
-			if err != nil {
-				log.Println("refresh swarm service failed:", swarmServiceId[:12], service.ID, err)
-				continue
-			}
-			log.Println("refreshed: swarm service ", swarmServiceId[:12], service.ID)
-		}
-	}
+	b.refreshServices(b.containerServices)
+	b.refreshServices(b.swarmVipServices)
+	b.refreshServices(b.swarmMgrServices)
 }
 
 func (b *Bridge) Sync(quiet bool) {
@@ -132,10 +137,11 @@ func (b *Bridge) Sync(quiet bool) {
 		}
 	}
 
-	if b.config.SwarmMode && b.config.SwarmModeManager {
-	  // docker daemons "/services" api endpoint is available on swarm master nodes only.
-		b.syncSwarmModeVipServicesOnMasterNode()
- 	}
+	// check whether swarm control is available (is this node a manager node currently)
+	b.swarmControlAvailable = b.isSwarmControlAvailable()
+
+	// sync all swarm dependent services
+	b.syncSwarmServices()
 
 	// Clean up services that were registered previously, but aren't
 	// acknowledged within registrator
@@ -172,7 +178,7 @@ func (b *Bridge) Sync(quiet bool) {
 
 	Outer:
 		for _, extService := range extServices {
-			matches := containerServiceIDPattern.FindStringSubmatch(extService.ID)
+			matches := containerSvcIDPattern.FindStringSubmatch(extService.ID)
 
 			if len(matches) != 3 {
 				// There's no way this was registered by us, so leave it
@@ -326,7 +332,7 @@ func (b *Bridge) newService(port ServicePort, isgroup bool) *Service {
 		service.Name = mapDefault(metadata, "name", defaultName)
 	}
 
-	// must match containerServiceIDPattern
+	// must match containerSvcIDPattern
 	service.ID = b.config.NodeId + ":" + container.Name[1:] + ":" + port.ExposedPort
 
 	if isgroup && !metadataFromPort["name"] {
@@ -400,62 +406,103 @@ func (b *Bridge) newService(port ServicePort, isgroup bool) *Service {
 }
 
 /*
- * Find registered swarm mode vip services by filtering all registered services from registration backend.
- * This is done by pattern matching the service ids.
- * Returns a map with the id of the swarm service as key and its appropriate registered services as value.
+ * Find registered services by filtering. This is done by using the given filter function. The filter function
+ * is called for each service. If it returns a string reference this reference will be used as key
+ * into the result map and the service will be appended to this map entry then.
  */
-func (b *Bridge) findRegisteredSwarmServices(registeredServices []*Service) map[string][]*Service {
+func filterServices(services []*Service, filter func(service *Service) *string) map[string][]*Service {
 	result := make(map[string][]*Service)
-	for _, registeredService := range registeredServices {
+	for _, service := range services {
+		if key := filter(service); key != nil {
+			if services, ok := result[*key]; ok {
+				services = append(services, service)
+				result[*key] = services
+			} else {
+				services := make([]*Service, 0)
+				services = append(services, service)
+				result[*key] = services
+			}
+		}
+	}
+	return result
+}
 
-		matches := swarmServiceIDPattern.FindStringSubmatch(registeredService.ID)
+/*
+ * Find registered swarm mode vip services by filtering. This is done by pattern matching the service ids.
+ * Returns a map with the id of the swarm service as key and its associated registered services as value.
+ */
+func (b *Bridge) filterSwarmVipServices(services []*Service) map[string][]*Service {
+
+	swarmVipServiceFilter := func(service *Service) *string {
+		matches := swarmVipSvcIDPattern.FindStringSubmatch(service.ID)
 
 		if len(matches) != 3 {
 			// this service wasn't registered as swarm mode service so we have to filter it here
-			continue
+			return nil
 		}
 
 		nodeId := matches[2]
 
 		if nodeId != b.config.NodeId {
 			// we can't deregister services that were registered on a different node
-			continue
+			return nil
 		}
 
-		swarmServiceId := matches[1]
-
-		if services, ok := result[swarmServiceId]; ok {
-			services = append(services, registeredService)
-			result[swarmServiceId] = services
-		} else {
-			services := make([]*Service, 0)
-			services = append(services, registeredService)
-			result[swarmServiceId] = services
-		}
+		return &matches[1]
 	}
 
-	return result
+	return filterServices(services, swarmVipServiceFilter)
+}
+
+func (b *Bridge) registeredServices() ([]*Service, error) {
+	registeredServices, err := b.registry.Services()
+	if err != nil {
+		log.Fatalf("can't get registered services: %v", err)
+	}
+	return registeredServices, err
 }
 
 /*
  * Iterates over all registered swarm mode vip services from registration backend and calls the given condition function.
  * If condition returns true it deregisters the appropriate service from the registration backend.
  */
-func (b *Bridge) deregisterRegisteredSwarmServices(condition func(swarmServiceId string, registeredService *Service) bool) {
-	registeredServices, err := b.registry.Services()
+func (b *Bridge) deregisterRegisteredSwarmVipServices(condition func(swarmServiceId string, registeredService *Service) bool) {
+	registeredServices, err := b.registeredServices()
+
 	if err != nil {
-		log.Fatal("cant get registered services: ", err)
 		return
 	}
 
-	for swarmServiceId, registeredServices := range b.findRegisteredSwarmServices(registeredServices) {
+	for swarmServiceId, registeredServices := range b.filterSwarmVipServices(registeredServices) {
 		for _, registeredService := range registeredServices {
 			if condition(swarmServiceId, registeredService) {
 				b.registry.Deregister(registeredService)
-				// remove swarm service from cache
-				delete(b.swarmServices, swarmServiceId)
+				// remove swarm vip service from cache
+				delete(b.swarmVipServices, swarmServiceId)
 			}
 		}
+	}
+}
+
+func (b *Bridge) syncSwarmServices() {
+	b.syncSwarmManagerService()
+	b.syncSwarmVipServices()
+}
+
+func (b *Bridge) SyncSwarmServices() {
+	b.Lock()
+	defer b.Unlock()
+
+	swarmControlAvailable := b.swarmControlAvailable
+
+	// check whether swarm control is available (is this node a manager node currently)
+	b.swarmControlAvailable = b.isSwarmControlAvailable()
+
+	swarmControlChanged := swarmControlAvailable != b.swarmControlAvailable
+
+	// sync swarm services only when swarm control availability of the own node has changes
+	if swarmControlChanged {
+		b.syncSwarmServices()
 	}
 }
 
@@ -465,13 +512,21 @@ func (b *Bridge) deregisterRegisteredSwarmServices(condition func(swarmServiceId
  * Sadly this is not possible atm for worker nodes as they don't provide the needed /services endpoint
  * nor it is possible to receive service events via the docker remote api.
  */
-func (b *Bridge) syncSwarmModeVipServicesOnMasterNode() {
-	log.Println("Syncing swarm mode vip services")
-	// get existing swarm services
-	servicefilters := map[string][]string{}
-	swarmServices, err := b.docker.ListServices(dockerapi.ListServicesOptions{Filters: servicefilters})
-	if err != nil {
-		log.Println("error listing swarm mode services:", err)
+func (b *Bridge) syncSwarmVipServices() {
+
+	log.Printf("Syncing swarm mode vip services. Swarm control available: %v", b.swarmControlAvailable)
+
+	var swarmServices []swarm.Service
+	var err error
+
+	// docker daemons "/services" api endpoint is available on swarm manager nodes only.
+	if b.swarmControlAvailable {
+		// get existing swarm services
+		servicefilters := map[string][]string{}
+		swarmServices, err = b.docker.ListServices(dockerapi.ListServicesOptions{Filters: servicefilters})
+		if err != nil {
+			log.Printf("error listing swarm mode services: %v", err)
+		}
 	}
 
 	swarmServicesMap := make(map[string]swarm.Service)
@@ -484,7 +539,7 @@ func (b *Bridge) syncSwarmModeVipServicesOnMasterNode() {
 	deregisterCondition := func(swarmServiceId string, registeredService *Service) bool {
 		if swarmService, ok := swarmServicesMap[swarmServiceId]; ok {
 			mode := swarmService.Spec.EndpointSpec.Mode
-			if b.config.ReplicasAware && *swarmService.Spec.Mode.Replicated.Replicas < uint64(1) {
+			if b.config.SwarmReplicasAware && *swarmService.Spec.Mode.Replicated.Replicas < uint64(1) {
 				log.Printf("removed: swarm vip service without replicas %s:%d ", registeredService.Name, registeredService.Port)
 				return true
 			} else if mode != swarm.ResolutionModeVIP {
@@ -499,7 +554,7 @@ func (b *Bridge) syncSwarmModeVipServicesOnMasterNode() {
 		}
 	}
 
-	b.deregisterRegisteredSwarmServices(deregisterCondition)
+	b.deregisterRegisteredSwarmVipServices(deregisterCondition)
 }
 
 func (b *Bridge) RegisterSwarmServiceById(aSwarmServiceId string) (*swarm.Service, error) {
@@ -517,7 +572,6 @@ func (b *Bridge) registerSwarmServiceById(aSwarmServiceId string) (*swarm.Servic
 		b.registerSwarmService(*swarmService)
 		return swarmService, err
 	} else {
-		log.Printf("cant inspect service: ", aSwarmServiceId, err)
 		return swarmService, err
 	}
 }
@@ -540,26 +594,30 @@ func (b *Bridge) deregisterSwarmServiceById(aSwarmServiceId string) {
 		}
 	}
 
-	b.deregisterRegisteredSwarmServices(deregisterCondition)
+	b.deregisterRegisteredSwarmVipServices(deregisterCondition)
 }
 
 func (b *Bridge) UpdateSwarmServiceById(aSwarmServiceId string) {
 	b.Lock()
 	defer b.Unlock()
 
-	log.Printf(">UpdateSwarmServiceById: ", aSwarmServiceId)
+	if aSwarmService := b.swarmVipServices[aSwarmServiceId]; aSwarmService == nil {
+		// serviceId is unknown so we won't update. this is an optimization for
+		// docker service events with unknown service ids (don't know the meaning of those events)
+		return
+	}
 
 	// registers service eventually (because replicas>0) if currently not registered (because replicas=0)
 	swarmService, err := b.registerSwarmServiceById(aSwarmServiceId)
 
 	if err != nil {
-		log.Printf("can't register swarm service by id: ", aSwarmServiceId)
+		log.Printf("can't register swarm service %s by id: %v", aSwarmServiceId, err)
 		return
 	}
 
 	deregisterCondition := func(swarmServiceId string, registeredService *Service) bool {
 		mode := swarmService.Spec.EndpointSpec.Mode
-		if swarmServiceId == aSwarmServiceId && b.config.ReplicasAware && *swarmService.Spec.Mode.Replicated.Replicas < uint64(1) {
+		if swarmServiceId == aSwarmServiceId && b.config.SwarmReplicasAware && *swarmService.Spec.Mode.Replicated.Replicas < uint64(1) {
 			log.Printf("removed: swarm vip service without replicas %s:%d ", registeredService.Name, registeredService.Port)
 			return true
 		} else if mode != swarm.ResolutionModeVIP {
@@ -570,7 +628,7 @@ func (b *Bridge) UpdateSwarmServiceById(aSwarmServiceId string) {
 		}
 	}
 
-	b.deregisterRegisteredSwarmServices(deregisterCondition)
+	b.deregisterRegisteredSwarmVipServices(deregisterCondition)
 }
 
 func (b *Bridge) registerSwarmService(swarmService swarm.Service) {
@@ -579,7 +637,7 @@ func (b *Bridge) registerSwarmService(swarmService swarm.Service) {
 		// DNSrr service will be handled as container service (see Sync())
 		if mode == swarm.ResolutionModeVIP {
 			if (len(swarmService.Endpoint.VirtualIPs) > 0) {
-				if b.config.ReplicasAware {
+				if b.config.SwarmReplicasAware {
 					if *swarmService.Spec.Mode.Replicated.Replicas > uint64(0) {
 						b.registerSwarmVipServices(swarmService)
 					}
@@ -697,14 +755,14 @@ func (b *Bridge) registerSwarmVipServicePorts(swarmService swarm.Service, inside
 			portMeta = make(map[string]string)
 		}
 
-		// must match swarmServiceIDPattern
-		serviceID := "swarm:" + swarmService.ID + ":" + b.config.NodeId + ":" + strconv.Itoa(targetPort)
+		// must match swarmVipSvcIDPattern
+		serviceID := "swarm-vip-svc:" + swarmService.ID + ":" + b.config.NodeId + ":" + strconv.Itoa(targetPort)
 
 		services = append(services, b.registerSwarmVipServicePort(serviceID, serviceName, portMeta, inside, vip, int(portNum), port.Protocol))
 	}
 
 	// cache registered swarm services
-	b.swarmServices[swarmService.ID] = services
+	b.swarmVipServices[swarmService.ID] = services
 
   log.Printf("registered %d services for swarm service %s ", len(services), swarmService.ID)
 }
@@ -804,4 +862,98 @@ func (b *Bridge) shouldRemove(containerId string) bool {
 		return true
 	}
 	return false
+}
+
+func (b *Bridge) isSwarmControlAvailable() bool {
+	dockerInfo, err := b.docker.Info()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return dockerInfo.Swarm.ControlAvailable
+}
+
+/*
+ * Find registered swarm mode manager services by filtering. This is done by pattern matching the service ids.
+ * Returns a map with the id of the swarm node as key and its associated registered service(s) as value.
+ */
+func (b *Bridge) filterSwarmManagerServices(services []*Service) map[string][]*Service {
+
+	swarmVipServiceFilter := func(service *Service) *string {
+		matches := swarmMgrSvcIDPattern.FindStringSubmatch(service.ID)
+
+		if len(matches) != 2 {
+			// this service wasn't registered as swarm manager service so we have to filter it here
+			return nil
+		}
+
+		nodeId := matches[1]
+
+		if nodeId != b.config.NodeId {
+			// we can't deregister services that were registered on a different node
+			return nil
+		}
+
+		return &nodeId
+	}
+
+	return filterServices(services, swarmVipServiceFilter)
+}
+
+/*
+ * Iterates over all registered swarm manager services from registration backend and calls the given condition function.
+ * If condition returns true it deregisters the appropriate service from the registration backend.
+ */
+func (b *Bridge) deregisterRegisteredSwarmManagerServices(condition func(nodeId string, registeredService *Service) bool) {
+	registeredServices, err := b.registeredServices()
+
+	if err != nil {
+		return
+	}
+
+	for nodeId, registeredServices := range b.filterSwarmManagerServices(registeredServices) {
+		for _, registeredService := range registeredServices {
+			if condition(nodeId, registeredService) {
+				b.registry.Deregister(registeredService)
+				// remove swarm manager service from cache
+				delete(b.swarmMgrServices, nodeId)
+			}
+		}
+	}
+}
+
+func (b *Bridge) syncSwarmManagerService() {
+
+	if b.config.SwarmManagerSvcName != "" {
+		log.Printf("Syncing swarm mode manager service. Swarm control available: %v", b.swarmControlAvailable)
+
+		if b.swarmControlAvailable {
+			service := new(Service)
+
+			// must match swarmMgrSvcIDPattern
+			service.ID = "swarm-mgr-svc:" + b.config.NodeId + ":2376"
+			service.Name = b.config.SwarmManagerSvcName
+			service.IP = b.config.HostIp
+			service.Port = 2376
+
+			err := b.registry.Register(service)
+			if err != nil {
+				log.Printf("register swarm manager service %s failed: %v", service.Name, err)
+			} else {
+				log.Printf("registered swarm manager service: %s", service.Name)
+			}
+		}
+	}
+
+	deregisterCondition := func(nodeId string, registeredService *Service) bool {
+		remove := !b.swarmControlAvailable
+		if remove {
+			log.Printf("removed: swarm manager service %s:%d ", registeredService.Name, registeredService.Port)
+		}
+		return remove
+	}
+
+	b.deregisterRegisteredSwarmManagerServices(deregisterCondition)
+
 }
