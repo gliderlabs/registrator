f9ada5ef4d34aa0009d1d047b2650e1d728cbec6
Alex Laverty <alex.laverty@cba.com.au>
2019-11-21 09:30:08 +1100

    fix: Convert from check.Script to check.Args for Consul API changes

--- consul/consul.go
+++ consul/consul.go
@@ -111,9 +111,9 @@ func (r *ConsulAdapter) buildCheck(service *bridge.Service) *consulapi.AgentServ
 			check.Method = method
 		}
 	} else if cmd := service.Attrs["check_cmd"]; cmd != "" {
-		check.Args = []string{"check-cmd", service.Origin.ContainerID[:12], service.Origin.ExposedPort, cmd}
+		check.Args = strings.Split(fmt.Sprintf("check-cmd %s %s %s", service.Origin.ContainerID[:12], service.Origin.ExposedPort, cmd), " ")
 	} else if script := service.Attrs["check_script"]; script != "" {
-		check.Args = []string{r.interpolateService(script, service)}
+		check.Args = strings.Split(r.interpolateService(script, service), " ")
 	} else if ttl := service.Attrs["check_ttl"]; ttl != "" {
 		check.TTL = ttl
 	} else if tcp := service.Attrs["check_tcp"]; tcp != "" {
@@ -124,7 +124,7 @@ func (r *ConsulAdapter) buildCheck(service *bridge.Service) *consulapi.AgentServ
 	} else {
 		return nil
 	}
-	if len(check.Args) != 0 || check.HTTP != "" || check.TCP != "" {
+	if len(check.Args) > 0 || check.HTTP != "" || check.TCP != "" {
 		if interval := service.Attrs["check_interval"]; interval != "" {
 			check.Interval = interval
 		} else {
